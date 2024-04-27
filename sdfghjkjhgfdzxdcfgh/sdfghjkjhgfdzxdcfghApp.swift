dc2827a981445a8bca22939f46e34003e5c721aa
Pawel Powroznik <ppowroznik@gmail.com>
2024-04-27 17:04:21 +0200

    Initial Commit

---
+++ sdfghjkjhgfdzxdcfgh/sdfghjkjhgfdzxdcfghApp.swift
@@ -0,0 +1,17 @@
+//
+//  sdfghjkjhgfdzxdcfghApp.swift
+//  sdfghjkjhgfdzxdcfgh
+//
+//  Created by Paweł Powroźnik on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct sdfghjkjhgfdzxdcfghApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
